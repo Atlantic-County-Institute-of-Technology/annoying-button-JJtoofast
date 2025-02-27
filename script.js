6fe1daa41e57cd334d6b557aabfc6ecd9e555d8b
adai9993 <adai9993@student.acitech.org>
2025-02-27 09:15:55 -0500

    -completed button and coding -completed the button itself and the coding

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const button1 = document.getElementById("button1");
 
-button1.addEventListener("click",yell_at_user);
+button1.addEventListener("click", yell_at_user);
 
 function yell_at_user() {                                                                                                        
-    alert("STOP PRESSING THE BUTTON")
+    alert("STOP PRESSING THE BUTTON"); 
+    
 }
