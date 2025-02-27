36a72d2bbdaf1b658a5ff19a0283c034007dd8af
adai9993 <adai9993@student.acitech.org>
2025-02-27 09:16:49 -0500

    -changed the saying on the alert

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const button1 = document.getElementById("button1");
 button1.addEventListener("click", yell_at_user);
 
 function yell_at_user() {                                                                                                        
-    alert("STOP PRESSING THE BUTTON"); 
+    alert("You got trolled buddie"); 
     
 }
