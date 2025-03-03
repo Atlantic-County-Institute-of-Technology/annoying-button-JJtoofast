dd07314803b536c26ecddfe082606bb5c3f03a8a
adai9993 <adai9993@student.acitech.org>
2025-03-03 09:08:13 -0500

    -added changes to my button changes -got my button to say different words -changed the shading on the button when pressing it in -and added the original video of think fast chucklenuts to the ending

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
-const button1 = document.getElementById("button1");
+let text = document.getElementById("text");
+let button1= document.getElementById("button1")
+
+let clicks=0;
 
 button1.addEventListener("click", yell_at_user);
 
-function yell_at_user() {                                                                                                        
-    alert("You got trolled buddie"); 
-    
-}
+function yell_at_user() {  
+    clicks++;     
+    if( clicks == 1 ){ 
+        text.innerHTML = "Hey! Stop that!";
+    }                                                                                              
+    else if( clicks == 5 ) {
+        text.innerHTML = "I SAID STOP!";
+    }     
+    else if( clicks == 10 ) {
+        text.innerHTML = "Seriously Your such a child !";
+    }       
+    else if( clicks == 15 ) {
+        text.innerHTML = " IM GOING TO FIND YOU !";
+    }       
+    else if( clicks == 20 ) {
+        text.innerHTML = " IS THIS YOUR ADRESS AND IP ####### #### ######  !";
+    }       
+
+    else if( clicks == 25 ) {
+        text.innerHTML = " IM 100 YARDS OUT OF YOUR CURRENT LOCATION AND IM CLOSING IN FAST !";
+    }       
+    else if( clicks == 30 ) {
+        text.innerHTML = " IM OUTSIDE YOUR DOOR OPEN UP NOW !";
+    }       
+    else if( clicks == 35 ) {
+        text.innerHTML = " IM IN YOUR BEDROOM WAKEY WAKEY !";
+    }
+     else if( clicks == 40 ) {
+            text.innerHTML = " THINK FAST CHUCKLEHEAD !";
+              
+     }
+        
+    }
+
+
+
